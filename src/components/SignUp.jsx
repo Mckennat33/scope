5fc54cc5e0706c2b34782b9d339192214a1a7146
tom mckenna <thomasmckenna12@gmail.com>
2026-01-02 18:09:46 -0600

    added div and css to the user signup information

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -20,77 +20,73 @@ function handleSubmitForm(e) {
             <div className='sign-up-container'>
                 <h1>Create Your Account</h1>
                 <div className='form-container'>
-                    <form action="" onSubmit={handleSubmitForm}>
-                        <div>
-                            <label htmlFor="username"></label>
-                            <input type="text" name="username" id="username" placeholder='Username' 
-                                onChange={(e) => {setUserName(e.target.value)}}
-                            />
-                        </div>
-                        <div>
-                            <label htmlFor="email"></label>
-                            <input type="email" name="email" id="email" placeholder='Email'
-                                onChange={(e) => {setEmail(e.target.value)}}
-                            />
-                        </div>
-                        <div>
-                            <label htmlFor=""></label>
-                            <input type="password" name='password' id='password' placeholder='Password'
-                                onChange={(e) => {setPassword(e.target.value)}}
-                            />
+                    <form className='form' action="" onSubmit={handleSubmitForm}>
+                        <div className='user-signup'>
+                            <div>
+                                <label htmlFor="username"></label>
+                                <input type="text" name="username" id="username" placeholder='Username' 
+                                    onChange={(e) => {setUserName(e.target.value)}}
+                                    />
+                            </div>
+                            <div>
+                                <label htmlFor="email"></label>
+                                <input type="email" name="email" id="email" placeholder='Email'
+                                    onChange={(e) => {setEmail(e.target.value)}}
+                                    />
+                            </div>
+                            <div>
+                                <label htmlFor=""></label>
+                                <input type="password" name='password' id='password' placeholder='Password'
+                                    onChange={(e) => {setPassword(e.target.value)}}
+                                    />
+                            </div>
                         </div>
                         <div className='tier-container'>
 
                         </div>
 
-                <div className='tier-container'>
-                    <div className='tier-free'>
-                        <div className='free-header'>
-                            <h4>Free</h4>
+                        <div className='tier-container'>
+                            <div className='tier-free'>
+                                <div className='free-header'>
+                                    <h4>Free</h4>
+                                </div>
+                                <div className='free-notes'>
+                                    <p>3 Articles per day</p>
+                                    <p>Trending News access</p>
+                                    <p>Limited comments</p>
+                                    <p>Basic Newsletter</p>
+                                    <button>Select Free</button>
+                                </div>
+                            </div>
+                            <div className='tier-standard'>
+                                <div className='standard-header'>
+                                    <h4>Standard</h4>
+                                </div>
+                                <div className='standard-notes'>
+                                    <p>10 articles per day</p>
+                                    <p>Trending & reccomended</p>
+                                    <p>Unlimited comments</p>
+                                    <p>Daily newsletter</p>
+                                    <button>Select Standard</button>
+                                </div>
+                            </div>
+                            <div className='tier-premium'>
+                                <div className='premium-header'>
+                                    <h4>Premium</h4>
+                                </div>
+                                <div className='premium-notes'>
+                                    <p>Umlimted articles</p>
+                                    <p>Premium content</p>
+                                    <p>Comments & save articles</p>
+                                    <p>Exlusive newsletter</p>
+                                    <button>Select Premium</button>
+                                </div>
+                            </div>
                         </div>
-                        <div className='free-notes'>
-                            <p>3 Articles per day</p>
-                            <p>Trending News access</p>
-                            <p>Limited comments</p>
-                            <p>Basic Newsletter</p>
-                            <button>Select Free</button>
-                        </div>
-                    </div>
-                    <div className='tier-standard'>
-                        <div className='standard-header'>
-                            <h4>Standard</h4>
-                        </div>
-                        <div className='standard-notes'>
-                            <p>10 articles per day</p>
-                            <p>Trending & reccomended</p>
-                            <p>Unlimited comments</p>
-                            <p>Daily newsletter</p>
-                            <button>Select Standard</button>
-                        </div>
-                    </div>
-                    <div className='tier-premium'>
-                        <div className='premium-header'>
-                            <h4>Premium</h4>
-                        </div>
-                        <div className='premium-notes'>
-                            <p>Umlimted articles</p>
-                            <p>Premium content</p>
-                            <p>Comments & save articles</p>
-                            <p>Exlusive newsletter</p>
-                            <button>Select Premium</button>
-                        </div>
-                    </div>
-                </div>
 
 
                     </form>
                 </div>
-
-
-
-
-
-
             </div>
         </div>
         </>
